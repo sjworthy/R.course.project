81ac36e541fdc0be4461829715591d0236dc74ef
sjworthy <31520696+sjworthy@users.noreply.github.com>
2017-10-11 13:52:59 -0400

    mean and sd based on weighted abundance cdm

---
+++ Range.Richness.wf/Trait.matrix/Wt.mean.sd.R
@@ -0,0 +1,17 @@
+#Determine mean trait values for each plot using abundance weighted cdm
+#function that calculates mean for the first column in the trait matrix
+for species that are present in the plot
+
+weighted.mean.function=function(x){
+weighted.mean(tdm[names(x[x >0]), 1], x[x>0])
+}
+apply(cdm.ra, MARGIN=1, weighted.mean.function)
+
+#Determine standard deviation of trait values for each plot
+#function that calculates standard deviation for the first column in the trait matrix
+for species that are present in the plot
+
+weighted.sd.function=function(x){
+wt.sd(traits[names(x[x>0]),1], x[x>0])
+}
+apply(cdm.ra, MARGIN=1, wt.sd.function)
