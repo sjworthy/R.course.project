c44de777874847d4049d16cac8ef9bc7f59bbc45
sjworthy <31520696+sjworthy@users.noreply.github.com>
2017-10-25 10:46:58 -0400

    Redo cdm
    Made community data matrix with final data set

--- Range.Richness.wf/Comm.Matrix/Comm.matrix.R
+++ Range.Richness.wf/Comm.Matrix/Comm.matrix.R
@@ -1,14 +1,13 @@
 #Make a community data matrix with number of species in each plot
-that are in each quantile
 #Make a blank community data matrix
-#5 columns, one for each quantile
+#111 columns, one for each species
 #16 rows, one for each plot in the transect (250m-1000m)
 
-cdm=matrix(data=NA, ncol=5, nrow=16)
+cdm=matrix(data=NA, ncol=111, nrow=16)
 
 #Add column names
 
-colnames(cdm)=c("Q1", "Q2", "Q3", "Q4", "Q5")
+colnames(cdm)=barone.ranges$SpeciesCode
 
 #Add row names
 
@@ -19,18 +18,20 @@ rownames(cdm)=c(250, 300, 350, 400, 450, 500, 550, 600, 650, 700,750, 800, 850, 
 listy=c(250, 300, 350, 400, 450, 500, 550, 600, 650, 700,750, 800, 850, 900, 950, 1000)
 
 #Fill in the community data matrix
-#A for loop for that loops through for each element in listy, so each elevation
+#A for loop that loops through for each element in listy, so each elevation
 #Subset the abundance data set by the plot elevation
-#Take that subsetted object and subset it again while removing duplicated names
-#Take that object and use the table function to summarize the number
-of species in each quantile based on the Quantile column
-#Place those values into row i
+#Take that subsetted object and table the SpeciesCode column
+#Take that object and place those values into row i
 
 for(i in 1:length(listy)){
-elevation=subset(Sonadora, Sonadora$plotElevation==listy[i])
-sp.name=subset(elevation, !duplicated(elevation$stemSpeciesCode))
-cdm[i,]=table(sp.name$Quantile)
+elevation=subset(sonadora, sonadora$plotElevation==listy[i])
+output=table(elevation$stemSpeciesCode)
+cdm[i,]=output
 }
+
+#To get a presence/absence community data matrix
+
+pa.cdm=decostand(cdm, method="pa")
 
 #to get narrow ranging species into a community data matrix
 #to be a narrow ranging species, range must be < mean
