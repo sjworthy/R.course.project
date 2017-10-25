4fccb4e7921c13d7080fc430e834e4fdfd987743
sjworthy <31520696+sjworthy@users.noreply.github.com>
2017-10-25 10:47:56 -0400

    Ran PCA

--- Range.Richness.wf/Trait.matrix/PCA.R
+++ Range.Richness.wf/Trait.matrix/PCA.R
@@ -1,12 +1,12 @@
 #perform a PCA on traits
-pca=princomp(traits.scaled)
+pca=princomp(tdm.scaled)
 
 #determine the proportion of the total variance explained by each axis
 #included axes that explain over 90-95% of the variance
 summary(pca)
 
 #determine what traits are most heavily weighted on these axes
-print(pc$loadings, cutoff=0.001)
+print(pca$loadings, cutoff=0.001)
 
 #extract where each species lands on the PC axes that
 make up the majority of the variance and use this to 
@@ -18,3 +18,6 @@ rownames(pc.scores)=rownames(traits)
 
 #calculate the multivariate Euclidean distance betwee all species
 pc.dist.mat=dist(pc.scores, method="euclidean")
+
+
+
