1baec78264401aea2124876805ffdaab18484813
sjworthy <31520696+sjworthy@users.noreply.github.com>
2017-10-25 10:48:08 -0400

    log transform traits

--- Range.Richness.wf/Trait.matrix/traits.transformed.R
+++ Range.Richness.wf/Trait.matrix/traits.transformed.R
@@ -1,4 +1,6 @@
 #transform trait data to approximate a normal distribution
 #scale the data to approximately a mean of 0 and unit variance
 
-traits.scaled=apply(log(traits), MARGIN=2, scale)
+tdm.scaled=apply(log(tdm), MARGIN=2, scale)
+row.names(tdm.scaled)=row.names(tdm)
+colnames(tdm.scaled)=c("log.height.ft", "log.la", "log.sla", "log.wood", "log.n", "log.p")
