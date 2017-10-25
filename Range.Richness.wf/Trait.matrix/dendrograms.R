7979b5278789375c81e31e4d34e0552586d84f72
sjworthy <31520696+sjworthy@users.noreply.github.com>
2017-10-25 11:23:28 -0400

    dendrograms

--- Range.Richness.wf/Trait.matrix/dendrograms.R
+++ Range.Richness.wf/Trait.matrix/dendrograms.R
@@ -10,6 +10,32 @@ dendro=hclust(dist.matrix, method="average")
 
 #Generate dendrogram for a single trait
 #Must assign rownames or the resulting dendrogram won't have species names
-trait.1=as.matrix(traits[,1])
-rownames(trait.1)=rownames(traits)
-dendro.trait.1=hclust(dist(trait.1, method="euclidean"), method="average")
+log.height=as.matrix(tdm.scaled[,1])
+rownames(log.height)=rownames(tdm.scaled)
+height.dendro=hclust(dist(log.height, method="euclidean"), method="average")
+plot(height.dendro, cex=0.4)
+
+log.la=as.matrix(tdm.scaled[,2])
+rownames(log.la)=rownames(tdm.scaled)
+la.dendro=hclust(dist(log.la, method="euclidean"), method="average")
+plot(la.dendro, cex=0.4)
+
+log.sla=as.matrix(tdm.scaled[,3])
+rownames(log.sla)=rownames(tdm.scaled)
+sla.dendro=hclust(dist(log.sla, method="euclidean"), method="average")
+plot(sla.dendro, cex=0.4)
+
+log.wood=as.matrix(tdm.scaled[,4])
+rownames(log.wood)=rownames(tdm.scaled)
+wood.dendro=hclust(dist(log.wood, method="euclidean"), method="average")
+plot(wood.dendro, cex=0.4)
+
+log.n=as.matrix(tdm.scaled[,5])
+rownames(log.n)=rownames(tdm.scaled)
+n.dendro=hclust(dist(log.n, method="euclidean"), method="average")
+plot(n.dendro, cex=0.4)
+
+log.p=as.matrix(tdm.scaled[,6])
+rownames(log.p)=rownames(tdm.scaled)
+p.dendro=hclust(dist(log.p, method="euclidean"), method="average")
+plot(p.dendro, cex=0.4)
